8e320404aff5fd69b169dd1b23b9616f44fa6767
simareeno <designer@5.45.224.246-red.dhcp.yndx.net>
2015-06-20 22:17:13 +0300

    все целиком

--- gulpfile.js
+++ gulpfile.js
@@ -9,26 +9,40 @@ var autoprefix = new AutoPrefixPlugin({
 	browsers: ['last 2 versions', 'opera >= 12']
 });
 
-var DEST_DEV = './out/dev';
-var DEST_PROD = './out/prod';
-
-var STYLES = './styles/*.less';
+var DEST_PROD = './out';
+var DEST_STYLES = './out/styles';
+var DEST_IMG = './out/img';
 
 gulp.task('styles', function() {
-	return gulp.src(STYLES)
+	return gulp.src('src/styles/*.less')
 		.pipe(plumber())
 		.pipe(less({
 			plugins: [autoprefix]
 		}))
-		.pipe(gulp.dest(DEST_DEV))
+		.pipe(gulp.dest(DEST_STYLES));
+});
+
+gulp.task('stylescss', function() {
+	return gulp.src('src/styles/*.css')
 		.pipe(minifyCSS())
-		.pipe(gulp.dest(DEST_PROD));
+		.pipe(gulp.dest(DEST_STYLES));
 });
 
-gulp.task('watch', function () {
-	watch(STYLES, function() {
-		gulp.start('styles');
-	});
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+	.pipe(gulp.dest(DEST_PROD));
 });
 
-gulp.task('default', ['styles']);
+gulp.task('img', function() {
+  return gulp.src('src/img/**/*')
+	.pipe(gulp.dest(DEST_IMG));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/img/**/*', ['img']);
+  gulp.watch('src/*.html', ['html']);
+  gulp.watch('src/styles/*.less', ['styles']);
+  gulp.watch('src/styles/*.css', ['stylescss']);
+});
+
+gulp.task('default', ['html', 'styles', 'img', 'stylescss', 'watch']);
